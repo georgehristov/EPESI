39d77b1271b36acd7dc3777c26c44f13db07964d
Adam Bukowski <bukowski.adam@gmail.com>
2014-06-11 12:09:54 +0200

    Rewrite index file. Wrap all code into the class for better structure. Html5 compatible. Remove redirect to mobile.

--- index.php
+++ index.php
@@ -1,256 +1,264 @@
 <?php
+
 /**
  * Index file
  *
- * This file includes all 'include files', loads modules
- * and gets output of default module.
- * @author Paul Bukowski <pbukowski@telaxus.com>
- * @copyright Copyright &copy; 2006, Telaxus LLC
- * @license MIT
- * @version 1.0 
- * @package epesi-base
+ * Inits EpesiIndex object and displays index file.
+ *
+ * @author    Paul Bukowski <pbukowski@telaxus.com>
+ *            Adam Bukowski <abukowski@telaxus.com
+ * @copyright Copyright &copy; 2014, Telaxus LLC
+ * @license   MIT
+ * @version   1.0
+ * @package   epesi-base
  */
-if(version_compare(phpversion(), '5.0.0')==-1)
-	die("You are running an old version of PHP, php5 required.");
 
-if(trim(ini_get("safe_mode")))
-	die('You cannot use EPESI with PHP safe mode turned on - please disable it. Please notice this feature is deprecated since PHP 5.3 and will be removed in PHP 6.0.');
+/**
+ * Class EpesiIndex
+ *
+ * Includes all necessary files to start javascript and init EPESI.
+ * Print base html template with necessary styles and scripts.
+ *
+ * @author    Adam Bukowski <abukowski@telaxus.com>
+ * @copyright Copyright &copy; 2014, Telaxus LLC
+ * @license   MIT
+ * @version   1.0
+ * @package   epesi-base
+ */
+class EpesiIndex
+{
 
-define('_VALID_ACCESS',1);
-require_once('include/data_dir.php');
-if(!file_exists(DATA_DIR.'/config.php')) {
-	header('Location: setup.php');
-	exit();
-}
+    public function show()
+    {
+        $this->check_requirements();
 
-if(!is_writable(DATA_DIR))
-	die('Cannot write into "'.DATA_DIR.'" directory. Please fix privileges.');
+        $this->load_data_dir_path();
 
-// require_once('include/include_path.php');
-require_once('include/config.php');
-require_once('include/maintenance_mode.php');
-require_once('include/error.php');
-ob_start(array('ErrorHandler','handle_fatal'));
-require_once('include/database.php');
-require_once('include/variables.php');
-$cur_ver = Variable::get('version');
-if($cur_ver!==EPESI_VERSION) {
-	if(isset($_GET['up'])) {
-		require_once('update.php');
-		$retX = ob_get_clean();
-		if(trim($retX))
-			die($retX);
-		header('Location: index.php');
-		exit();
-	} else {
-		?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-<head>
-      <meta content="text/html; charset=utf-8" http-equiv="content-type">
-      <title>EPESI update</title>
-      <link href="setup.css" type="text/css" rel="stylesheet"/>
-      <meta name="robots" content="NOINDEX, NOARCHIVE">
-</head>
-<body>
-        <table id="banner" border="0" cellpadding="0" cellspacing="0">
-            <tr>
-                <td class="image">&nbsp;</td>
-                <td class="back">&nbsp;</td>
-            </tr>
-        </table>
-        <br>
-        <center>
-        <table id="main" border="0" cellpadding="0" cellspacing="0">
-            <tr>
-                <td>
-				Updating EPESI from version <?php echo $cur_ver; ?> to <?php echo EPESI_VERSION; ?>. This operation may take several minutes.
-				<a href="index.php?up=1">Click here to proceed</a>
-                </td>
-            </tr>
-        </table>
-        </center>
-        <br>
-        <center>
-        <span class="footer">Copyright &copy; 2014 &bull; <a href="http://www.telaxus.com">Telaxus LLC</a></span>
-        <br>
-        <p><a href="http://www.epesi.org"><img src="images/epesi-powered.png" alt="image" border="0"></a></p>
-        </center>
-</body>
-</html>
-<?php
-		ob_end_flush();
-		exit();
-	}
-}
+        if (!$this->epesi_installed()) {
+            $this->redirect_to_setup();
+        } else {
+            $this->load_and_show();
+        }
+    }
 
-$tables = DB::MetaTables();
-if(!in_array('modules',$tables) || !in_array('variables',$tables) || !in_array('session',$tables))
-	die('Database structure you are using is apparently out of date or damaged. If you didn\'t perform application update recently you should try to restore the database. Otherwise, please refer to EPESI documentation in order to perform database update.');
+    private function check_requirements()
+    {
+        $system_php_version = phpversion();
+        $required_php_version = '5.3';
+        if (version_compare(phpversion(), $required_php_version, '<')) {
+            die("You are running too old version of PHP. At least $required_php_version is required. You're using $system_php_version");
+        }
 
-require_once('include/misc.php');
+        if (trim(ini_get("safe_mode"))) {
+            die('You cannot use EPESI with PHP safe mode turned on - please disable it. Please notice this feature is deprecated since PHP 5.3 and will be removed in PHP 6.0.');
+        }
+    }
 
-if(IPHONE) {
-	if(!isset($_GET['force_epesi'])) {
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="robots" content="NOINDEX, NOARCHIVE">
-	<meta id="viewport" name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
-	<title><?php print(EPESI);?></title>
-	<link rel="stylesheet" href="libs/UiUIKit/stylesheets/iphone.css" />
-	<link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
-	<script type="text/javascript" charset="utf-8">
-		window.onload = function() {
-		  setTimeout(function(){window.scrollTo(0, 1);}, 100);
-		}
-	</script>
-</head>
+    private function load_data_dir_path()
+    {
+        define('_VALID_ACCESS', 1);
+        require_once('include/data_dir.php');
+    }
 
-<body>
-<div id="header">
-		<h1><?php print(EPESI);?></h1>
-</div>
+    private function epesi_installed()
+    {
+        return file_exists(DATA_DIR . '/config.php');
+    }
 
-Please choose <?php print(EPESI);?> version:<ul>
-<li><a href="mobile.php" class="white button">mobile</a><br>
-<li><a href="index.php?force_epesi=1" class="green button">desktop</a>
-</ul>
+    private function redirect_to_setup()
+    {
+        header('Location: setup.php');
+    }
 
-</body>
-</html>
-<?php
-		ob_end_flush();
-		exit();
-	}
-} elseif(detect_mobile_device()) {
-	header('Location: mobile.php');
-	exit();
-}
+    private function load_and_show()
+    {
+        require_once('include/config.php');
+        require_once('include/maintenance_mode.php');
+        require_once('include/error.php');
+        ob_start(array('ErrorHandler', 'handle_fatal'));
+        require_once('include/database.php');
+        require_once('include/variables.php');
+        require_once('include/misc.php');
 
-?>
-<!DOCTYPE html>
-<html>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" type="image/png" href="images/favicon.png" />
-    <link rel="apple-touch-icon" href="images/apple-favicon.png" />
-    <title><?php print(EPESI);?></title>
-    <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
-    <meta name="robots" content="NOINDEX, NOARCHIVE">
-<?php
-	ini_set('include_path','libs/minify'.PATH_SEPARATOR.'.'.PATH_SEPARATOR.'libs'.PATH_SEPARATOR.ini_get('include_path'));
-	require_once('Minify/Build.php');
-	$jses = array('libs/prototype.js','libs/jquery-1.7.2.min.js','libs/jquery-ui-1.10.1.custom.min.js','libs/HistoryKeeper.js','libs/bootstrap/js/bootstrap.js', 'include/epesi.js');
-	$jsses_build = new Minify_Build($jses);
-	$jsses_src = $jsses_build->uri('serve.php?'.http_build_query(array('f'=>array_values($jses))));
-?>
-		<script type="text/javascript" src="<?php print($jsses_src)?>"></script>
-<?php
-	$csses = array('libs/jquery-ui-1.10.1.custom.min.css');
-	$csses_build = new Minify_Build($csses);
-	$csses_src = $csses_build->uri('serve.php?'.http_build_query(array('f'=>array_values($csses))));
-?>
-		<link type="text/css" href="<?php print($csses_src)?>" rel="stylesheet">
-        <!-- Include main style in the standard way due to relative paths in css -->
-		<link type="text/css" href="style/css/epesi.css" rel="stylesheet">
-		<style type="text/css">
-			<?php if (DIRECTION_RTL) print('body { direction: rtl; }'); ?>
-			#epesiStatus {
-  				/* Netscape 4, IE 4.x-5.0/Win and other lesser browsers will use this */
-  				position: fixed;
-  				left: 50%; top: 30%;
-                margin-left: -280px;
-  				/* all */
-  				/*background-color: #e6ecf2;*/
-  				background-color: white;
-				border: 5px solid #336699;
-				visibility: hidden;
-				width: 560px;
-				text-align: center;
-				vertical-align: middle;
-				z-index: 2002;
-                color: #336699;
-				overflow: hidden;
-				
-				/* css3 shadow border*/
-				-webkit-box-shadow: 1px 1px 15px black;
-				-moz-box-shadow: 1px 1px 15px black;
-				box-shadow: 1px 1px 15px black;
-				/* end css3 shadow border*/
-				
-				/* border radius */
-				-webkit-border-radius: 6px;
-				-moz-border-radius: 6px;
-				border-radius: 6px;
-				/* end border radius */
-			}
-			#epesiStatus table {
-				color: #336699;
-				font-weight: bold;
-				font-family: Tahoma, Verdana, Vera-Sans, DejaVu-Sans;
-				font-size: 11px;
-				border: 5px solid #FFFFFF;
+        if ($this->update_available()) {
+            $this->update_process();
+        } else {
+            $this->show_index();
+        }
+
+        $content = ob_get_clean();
+
+        require_once('libs/minify/HTTP/Encoder.php');
+        $he = new HTTP_Encoder(array('content' => $content));
+        if (MINIFY_ENCODE) {
+            $he->encode();
+        }
+        $he->sendAll();
+    }
+
+    private function update_available()
+    {
+        $installed_version = Variable::get('version');
+        return $installed_version !== EPESI_VERSION;
+    }
+
+    private function update_process()
+    {
+        if (isset($_GET['up'])) {
+            require_once('update.php');
+            $retX = ob_get_clean();
+            if (trim($retX)) {
+                die($retX);
             }
+            header('Location: index.php');
+        } else {
+            $title = EPESI . " update";
+            $this->print_html_header($title);
+            $this->print_update_message();
+            $this->print_html_footer(true);
+        }
+    }
 
-		</style>
-		<?php print(TRACKING_CODE); ?>
-	</head>
-	<body <?php if (DIRECTION_RTL) print('class="epesi_rtl"'); ?> >
+private function print_html_header($title)
+{
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php print($title); ?></title>
+        <link rel="icon" type="image/png" href="images/favicon.png"/>
+        <!-- Apple icon -->
+        <link rel="apple-touch-icon" href="images/apple-favicon.png"/>
+        <!-- Disable Skype to parse html -->
+        <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE"/>
+        <meta name="robots" content="NOINDEX, NOARCHIVE">
 
-		<div id="body_content">
-			<div id="main_content" style="display:none;"></div>
-			<div id="debug_content" style="padding-top:97px;display:none;">
-				<div class="button" onclick="$('error_box').innerHTML='';$('debug_content').style.display='none';">Hide</div>
-				<div id="debug"></div>
-				<div id="error_box"></div>
-			</div>
-			
-			<div id="epesiStatus">
-				<table cellspacing="0" cellpadding="0" border="0" style="width: 100%;">
-					<tr>
-						<td><img src="images/logo.png" alt="logo" width="550" height="200" border="0"></td>
-					</tr>
-					<tr>
-						<td style="text-align: center; vertical-align: middle; height: 30px;"><span id="epesiStatusText"><?php print(STARTING_MESSAGE);?></span></td>
-					</tr>
-					<tr>
-						<td style="text-align: center; vertical-align: middle; height: 30px;"><img src="images/loader.gif" alt="loader" width="256" height="10" border="0"></td>
-					</tr>
-				</table>
-			</div>	
-		</div>
-        <?php 
+        <link type="text/css" href="libs/jquery-ui-1.10.1.custom.min.css"
+              rel="stylesheet">
+        <link type="text/css" href="style/css/epesi.css" rel="stylesheet">
+        <?php if (DIRECTION_RTL) { ?>
+            <style type="text/css">
+                body {
+                    direction: rtl;
+                }
+            </style>
+        <?php
+        }
+        // serve.php reduces transfer with gzip compression
+        ini_set('include_path', 'libs/minify' . PATH_SEPARATOR . '.' . PATH_SEPARATOR . 'libs' . PATH_SEPARATOR . ini_get('include_path'));
+        require_once('Minify/Build.php');
+        $jses = array('libs/prototype.js',
+                      'libs/jquery-1.7.2.min.js',
+                      'libs/jquery-ui-1.10.1.custom.min.js',
+                      'libs/HistoryKeeper.js',
+                      'style/js/bootstrap.min.js',
+                      'include/epesi.js');
+        $jsses_build = new Minify_Build($jses);
+        $jsses_src = $jsses_build->uri('serve.php?' . http_build_query(array('f' => array_values($jses))));
+        ?>
+        <script type="text/javascript"
+                src="<?php print($jsses_src) ?>"></script>
+        <style type="text/css">
+            <?php if (DIRECTION_RTL) {print('body { direction: rtl; }');} ?>
+        </style>
+        <?php print(TRACKING_CODE); ?>
+    </head>
+    <body<?php if (DIRECTION_RTL) {
+        print(' class="epesi_rtl"');
+    } ?>>
+    <?php
+    }
+
+    private function print_update_message()
+    {
+        $installed_version = Variable::get('version'); ?>
+        <div>
+            Updating EPESI from version <?php echo $installed_version; ?>
+            to <?php echo EPESI_VERSION; ?>. This operation may take several
+            minutes.
+            <a href="index.php?up=1">Click here to proceed</a>
+        </div> <?php
+    }
+
+    private function print_html_footer($with_copyright)
+    {
+    if ($with_copyright) {
+        ?>
+        <div class="footer">
+            <p>Copyright &copy; 2014 &bull; <a href="http://www.telaxus.com">Telaxus
+                    LLC</a></p>
+
+            <p><a href="http://www.epesi.org"><img
+                        src="images/epesi-powered.png" alt="EPESI powered"></a>
+            </p>
+        </div> <?php
+    } ?>
+    </body>
+    </html> <?php
+}
+
+    private function show_index()
+    {
+        $this->print_html_header(EPESI);
+        $this->print_body_content();
+        $this->print_html_footer(false);
+    }
+
+    private function print_body_content()
+    {
+        // TODO: rewrite this html
+        ?>
+        <div id="body_content">
+            <div id="main_content" style="display:none;"></div>
+            <div id="debug_content" style="padding-top:97px;display:none;">
+                <div class="button"
+                     onclick="$('error_box').innerHTML='';$('debug_content').style.display='none';">
+                    Hide
+                </div>
+                <div id="debug"></div>
+                <div id="error_box"></div>
+            </div>
+
+            <div id="epesiStatus">
+                <div><img src="images/logo.png" alt="logo"></div>
+                <div
+                    id="epesiStatusText"><?php print(STARTING_MESSAGE); ?></div>
+                <div id="epesiStatusLoader"><img src="images/loader.gif"
+                                                 alt="loader"></div>
+            </div>
+        </div>
+        <?php
         /*
          * init_js file allows only num_of_clients sessions. If there is image
          * with empty src="" browser will load index.php file, so we cannot
          * include init_js file directly because num_of_clients request will
          * reset our history and restart EPESI.
-         * 
+         *
          * Check here if request accepts html. If it does we can assume that
          * this is request for page and include init_js file which is faster.
          * If there is not 'html' in accept use script with src property.
          */
-        if(isset($_SERVER['HTTP_ACCEPT']) && stripos($_SERVER['HTTP_ACCEPT'], 'html') !== false) { ?>
-		<script type="text/javascript"><?php require_once 'init_js.php'; ?></script>
+        if (isset($_SERVER['HTTP_ACCEPT']) && stripos($_SERVER['HTTP_ACCEPT'], 'html') !== false) {
+            ?>
+            <script
+                type="text/javascript"><?php require_once 'init_js.php'; ?></script>
         <?php } else { ?>
-		<script type="text/javascript" src="init_js.php?<?php print(http_build_query($_GET));?>"></script>
+            <script type="text/javascript"
+                    src="init_js.php?<?php print(http_build_query($_GET)); ?>"></script>
         <?php } ?>
-        <noscript>Please enable JavaScript in your browser and let <?php print(EPESI);?> work!</noscript>
-		<?php if(IPHONE) { ?>
-		<script type="text/javascript">var iphone=true;</script>
-		<?php } ?>
-	</body>
-</html>
-<?php
-$content = ob_get_contents();
-ob_end_clean();
+        <noscript>
+            Please enable JavaScript in your browser and
+            let <?php print(EPESI); ?> work!
+        </noscript>
+        <?php if (IPHONE) { ?>
+        <script type="text/javascript">var iphone = true;</script>
+    <?php
+    }
+    }
+}
 
-require_once('libs/minify/HTTP/Encoder.php');
-$he = new HTTP_Encoder(array('content' => $content));
-if (MINIFY_ENCODE)
-	$he->encode();
-$he->sendAll();
-?>
+$index = new EpesiIndex();
+$index->show();
