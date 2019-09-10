d686641772d13a646f9608e710f9321fae431d04
Georgi Hristov <ghristov@gmx.de>
2019-09-10 14:08:26 +0200

    [FIX] dependency definition

--- modules/Base/Cron/CronInstall.php
+++ modules/Base/Cron/CronInstall.php
@@ -33,7 +33,7 @@ class Base_CronInstall extends ModuleInstall {
 			array('name'=>Base_LangInstall::module_name(),'version'=>0),
 			array('name'=>Base_AdminInstall::module_name(),'version'=>0),
 			array('name'=>Base_RegionalSettingsInstall::module_name(),'version'=>0),
-			array('name'=>Utils_GenericBrowserInstall::module_name(),'version'=>0));
+			array('name'=>Utils_GenericBrowserInstall::module_name(),'version'=>0),
 			array('name'=>Utils_TooltipInstall::module_name(),'version'=>0));
 	}
 	
